07e89974970cb8a082a6dc59aa43713366039ff3
HP <riyasawwalakhe@gmail.com>
2025-08-31 21:01:44 +0530

    Created a spring boot project

---
+++ src/main/java/com/vehisoc/VehiSocApplication.java
@@ -0,0 +1,13 @@
+package com.vehisoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehiSocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehiSocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vehisoc/VehiSocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vehisoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehiSocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
